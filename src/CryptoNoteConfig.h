f01bc85ece9482887525ddd0bf871fee5013831a
rdp-studio <67402215+rdp-studio@users.noreply.github.com>
2020-07-20 19:22:34 +0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "coin.rdpstudio.top:17217",
+  "121.196.183.114:17217",
 };
 
 struct CheckpointData {
